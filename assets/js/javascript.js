5ade18d294eb385bed893a6daf0fa5f5e24e9443
Flor Tello <flor.26071994@gmail.com>
2017-02-23 16:59:35 -0500

    Primer commit Mostrando Objetos

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,18 +1,18 @@
+function mostrarDatos(){
+	var array = [ { nombre : "Arabela", apellido : "Rojas", rol : "T.A", cumpleanios : "23 de enero" },
+				{ nombre : "Michelle", apellido : "Seguil", rol : "T.A", cumpleanios : "25 de noviembre" }, 
+				{ nombre : "Meche", apellido : "Zubieta", rol : "T.A", cumpleanios : "02 de marzo" }, 
+				{ nombre : "Papu", apellido : "Rivariola", rol : "Psicóloga", cumpleanios : "10 de octubre" }, 
+				{ nombre : "Gian", apellido : "Corzo", rol : "Profesor", cumpleanios : "23 de enero" } ];
 
-
- function convertirCelcius (){
-  var fahrenheit = document.getElementById('textFahrenheit').value;
-
- 	if(fahrenheit != null){
- 		document.getElementById('textCelcius').value = (9/5) * fahrenheit + 32;
- 	}
- 	
- }
- function convertirFahrenheit (){
-  var celcius = document.getElementById('textCelcius').value;
-  if(celcius != null){
- 	document.getElementById('textFahrenheit').value = (celcius - 32) * (5/9);
- 	}
- }
-
-
+	var mostrar = "<ul>";
+	array.forEach(function(e){
+		mostrar += "<div><li> Nombre: "+e.nombre+"<br /></li>"+
+				   "<li>Apellido: "+e.apellido+"<br /></li>"+
+				   "<li>Rol: "+e.rol+"<br /></li>"+
+				   "<li>Cumpleaños: "+e.cumpleanios+"<br /></li></div><br />";
+	});
+	mostrar += "</ul>";
+	document.write(mostrar);
+}
+mostrarDatos();
